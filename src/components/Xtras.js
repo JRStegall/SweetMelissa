93fb81e5f656eaa19012b6d812fc36f27b41a96e
Jeremy <jeremy.stegall77@gmail.com>
2021-10-18 10:14:16 -0400

    Add links to pics

--- src/components/Xtras.js
+++ src/components/Xtras.js
@@ -24,123 +24,199 @@ function Xtras() {
       <div className="row">
         <div className="col this">
           <div className="row pics">
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img src={BR} className="App-logo img-responsive" alt="Barry" />
-                <figcaption class="figure-caption text-right">
-                  Barry Richman - guitar
-                </figcaption>
+                <a
+                  href="https://www.BarryRichman.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Barry Richman - guitar
+                  </figcaption>
+                </a>
               </figure>
             </div>
 
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img src={JE} className="App-logo img-responsive" alt="Joel" />
-                <figcaption class="figure-caption text-right">
-                  Joel Edwards - keys
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/JoelEdwardsATL"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Joel Edwards - keys
+                  </figcaption>
+                </a>
               </figure>
             </div>
 
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={RJ}
                   className="App-logo img-responsive"
                   alt="Richie"
                 />
-                <figcaption class="figure-caption text-right">
-                  Richie Jones - drums
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/richiejonesdrummer"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Richie Jones - drums
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={DK}
                   className="App-logo img-responsive"
                   alt="Damian"
                 />
-                <figcaption class="figure-caption text-right">
-                  Damian Kapcala - keys
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/damian.kapcala"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Damian Kapcala - keys
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={TS}
                   className="App-logo img-responsive"
                   alt="Trenton"
                 />
-                <figcaption class="figure-caption text-right">
-                  Trenton Scicola - drums
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/trenton.sicola"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Trenton Scicola - drums
+                  </figcaption>
+                </a>
               </figure>
             </div>
 
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={Jesse}
                   className="App-logo img-responsive"
                   alt="Jessie"
                 />
-                <figcaption class="figure-caption text-right">
-                  Jesse Sunga - guitar
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/jessesunga"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Jesse Sunga - guitar
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img src={TN} className="App-logo img-responsive" alt="Tyler" />
-                <figcaption class="figure-caption text-right">
-                  Tyler Neal - guitar
-                </figcaption>
+                <a
+                  href="https://www.tylernealband.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Tyler Neal - guitar
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={MP}
                   className="App-logo img-responsive"
                   alt="Michael"
                 />
-                <figcaption class="figure-caption text-right">
-                  Michael Plunkett - guitar
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/michael.plunkett"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Michael Plunkett - guitar
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={AA}
                   className="App-logo img-responsive"
                   alt="Adrian"
                 />
-                <figcaption class="figure-caption text-right">
-                  Adrian Ash - drums
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/badashallstarteam"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <figcaption class="figure-caption text-right">
+                    Adrian Ash - drums
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img
                   src={Justin}
                   className="App-logo img-responsive"
                   alt="Justin"
                 />
-                <figcaption class="figure-caption text-right">
-                  Justin Beals - guitar
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/justin.beals"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Justin Beals - guitar
+                  </figcaption>
+                </a>
               </figure>
             </div>
-            <div className="col-4">
+            <div className="col-sm-8 col-lg-6">
               <figure className="figure">
                 <img src={AS} className="App-logo img-responsive" alt="Simon" />
-                <figcaption class="figure-caption text-right">
-                  Adam Simon - keys
-                </figcaption>
+                <a
+                  href="https://www.facebook.com/adamcsimon"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  <figcaption class="figure-caption text-right">
+                    Adam Simon - keys
+                  </figcaption>
+                </a>
               </figure>
             </div>
           </div>
