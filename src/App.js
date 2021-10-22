5aac10dbcf91590676ef5246bb1e13ce8bb9d439
Jeremy <jeremy.stegall77@gmail.com>
2021-10-22 12:18:13 -0400

    fucking redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navigate from "./components/Nav";
 import Main from "./components/Main";
 import Shows from "./components/Shows";
@@ -7,19 +8,21 @@ import SetList from "./components/SetList";
 import Xtras from "./components/Xtras";
 import Music from "./components/Music";
 import "./App.css";
-import "/public/_redirects";
+import "./_redirects";
 
 function App() {
   return (
     <div className="App">
-      <Navigate />
-      <Switch>
-        <Route path="/music" component={Music} />
-        <Route path="/gallery" component={Shows} />
-        <Route path="/events" component={SetList} />
-        <Route path="/friends" component={Xtras} />
-        <Route path="/" component={Main} />
-      </Switch>
+      <BrowserRouter>
+        <Navigate />
+        <Switch>
+          <Route path="/music" component={Music} />
+          <Route path="/gallery" component={Shows} />
+          <Route path="/events" component={SetList} />
+          <Route path="/friends" component={Xtras} />
+          <Route path="/" component={Main} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
