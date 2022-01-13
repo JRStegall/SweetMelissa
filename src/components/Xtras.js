cf32f5b194d116122b05279be4b7db482c4e1fad
Jeremy <jeremy@Jeremys-MacBook-Pro-2.local>
2022-01-13 13:23:18 -0500

    Update Xtrass

--- src/components/Xtras.js
+++ src/components/Xtras.js
@@ -63,29 +63,6 @@ function Xtras() {
                 </figure>
               </div>
 
-              {/* Trenton */}
-              <div className="col-sm-8 col-lg-6">
-                <figure className="figure">
-                  <img
-                    src={TS}
-                    className="App-logo img-responsive"
-                    alt="Trenton"
-                  />
-                  <a
-                    href="https://www.facebook.com/trenton.sicola"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    <figcaption class="figure-caption text-right">
-                      Trenton Scicola - drums
-                    </figcaption>
-                  </a>
-                  <hr />
-                  <p className="caption text-left"></p>
-                </figure>
-              </div>
-
               {/* BARRY */}
               <div className="col-sm-8 col-lg-6">
                 <figure className="figure">
@@ -167,6 +144,29 @@ function Xtras() {
                     Ralph Rodenberry
                   </p>
                   <hr />
+                </figure>
+              </div>
+
+              {/* Trenton */}
+              <div className="col-sm-8 col-lg-6">
+                <figure className="figure">
+                  <img
+                    src={TS}
+                    className="App-logo img-responsive"
+                    alt="Trenton"
+                  />
+                  <a
+                    href="https://www.facebook.com/trenton.sicola"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <figcaption class="figure-caption text-right">
+                      Trenton Scicola - drums
+                    </figcaption>
+                  </a>
+                  <hr />
+                  <p className="caption text-left"></p>
                 </figure>
               </div>
 
